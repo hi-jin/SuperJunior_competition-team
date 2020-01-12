2eca664f47b3751d58c0a2278b0f3cb2e2b82eda
hi-jin <crushed7@o.cnu.ac.kr>
2020-01-12 17:32:39 +0900

    add login

--- src/application/ClientInfo.java
+++ src/application/ClientInfo.java
@@ -5,6 +5,8 @@ import java.net.Socket;
 public class ClientInfo {
 	public static Socket socketInfo = null; //클라이언트 소켓 객체 저장
 	public static boolean connection = false; 
+	public static String userId;
+	public static String groupId;
 	
 	public static void setSocket(Socket sk){
 		socketInfo = sk;
--- src/application/RootController.java
+++ src/application/RootController.java
@@ -1,27 +1,39 @@
 package application;
 
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.io.PrintWriter;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
 
 public class RootController implements Initializable {
 
-	@FXML Label serverTest;
-	
 	Socket socket;
+
+	@FXML TextField idTextField;
+
+	@FXML Label error_msg;
+
+	@FXML Button loginButton;
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		
 		if(!ClientInfo.connection) { //socket connect를 최초 실행시 한 번만 실행되기 위함 , 소켓  객체 중복 생성 방지
 			
-		final String SERVER_IP = "172.30.1.14"; //local에서 테스트 중일 경우 자신의 IP 작성
+		final String SERVER_IP = "localhost"; //local에서 테스트 중일 경우 자신의 IP 작성
 
 		final int SERVER_PORT = 50000;
 		
@@ -29,16 +41,64 @@ public class RootController implements Initializable {
         
         try {
             socket.connect( new InetSocketAddress(SERVER_IP, SERVER_PORT) );
-            serverTest.setText("success connection to server"); //test code
+            error_msg.setText("success connection to server"); //test code
             
             ClientInfo.setSocket(socket);
+            
+            Thread listener = new Thread(() -> {
+            	try {
+					BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+					
+					String line;
+					String[] command;
+					while(true) {
+						line = in.readLine();
+						System.out.println("server=>" + line);
+						
+						command = line.split("/");
+						
+						switch(command[0]) {
+						case "login":
+							if(command[1].equals("1")) {
+								ClientInfo.userId = idTextField.getText();
+								ClientInfo.groupId = command[2];
+								Platform.runLater(() -> error_msg.setText("로그인 성공."));
+							} else if(command[1].equals("2")) {
+								login();
+							} else {
+								Platform.runLater(() -> error_msg.setText("다시 시도해주세요."));
+							}
+						}
+					}
+				} catch (IOException e) {
+					Platform.runLater(() -> error_msg.setText("서버 에러 혹은 인터넷 연결을 확인해주세요."));
+					Platform.runLater(() -> loginButton.setDisable(true));
+				}
+            });
+            listener.setDaemon(true);
+            listener.start();
         }
         
         catch (IOException e) {
-            e.printStackTrace();
+        	Platform.runLater(() -> error_msg.setText("서버 에러 혹은 인터넷 연결을 확인해주세요."));
+        	Platform.runLater(() -> loginButton.setDisable(true));
         }
 		
 	}
 
 }
+
+	@FXML public void login() {
+		if(!idTextField.getText().equals("")) {
+			Socket server = application.ClientInfo.socketInfo;
+			try {
+				PrintWriter out = new PrintWriter(new BufferedOutputStream(server.getOutputStream()));
+				out.println("login/" + idTextField.getText());
+				out.flush();
+			} catch (IOException e) {
+	        	error_msg.setText("서버 에러 혹은 인터넷 연결을 확인해주세요.");
+	        	loginButton.setDisable(true);
+			}
+		}
+	}
 }
