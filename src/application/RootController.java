a3e5cac1d81bb71af4ee32b9ae01039e2d584bea
hi-jin <crushed7@o.cnu.ac.kr>
2020-01-16 19:39:20 +0900

    1. 컨트롤러를 모아놓은 클래스 생성

--- src/application/RootController.java
+++ src/application/RootController.java
@@ -12,6 +12,7 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import data.ClientInfo;
+import data.Controllers;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -35,72 +36,13 @@ public class RootController implements Initializable {
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		
-		if(!ClientInfo.connection) { //socket connect를 최초 실행시 한 번만 실행되기 위함 , 소켓  객체 중복 생성 방지
-			
-		final String SERVER_IP = "localhost"; //local에서 테스트 중일 경우 자신의 IP 작성
-
-		final int SERVER_PORT = 40000;
-		
-        socket = new Socket();
+		Controllers.rootController = this;
         
-        try {
-            socket.connect( new InetSocketAddress(SERVER_IP, SERVER_PORT) );
-            error_msg.setText("success connection to server"); //test code
-            
-            ClientInfo.setSocket(socket);
-            
-            if(ClientInfo.userId != null && !ClientInfo.userId.equals("") ) {
-            	Platform.runLater(() -> idTextField.setText(ClientInfo.userId));
-            	Platform.runLater(() -> login());
-            }
-            
-            Thread listener = new Thread(() -> {
-            	try {
-					BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-					
-					String line;
-					String[] command;
-					while(true) {
-						line = in.readLine();
-						System.out.println("server=>" + line);
-						
-						command = line.split("/");
-						
-						switch(command[0]) {
-						case "login":
-							if(command[1].equals("1")) {
-								ClientInfo.userId = idTextField.getText();
-								ClientInfo.groupId = command[2];
-								Platform.runLater(() -> error_msg.setText("로그인 성공."));
-							} else if(command[1].equals("2")) {
-								login();
-							} else {
-								Platform.runLater(() -> error_msg.setText("다시 시도해주세요."));
-							}
-							break;
-						case "update":
-							// TODO 진척도를 업데이트 하도록 시킴.
-							break;
-						}
-					}
-				} catch (IOException e) {
-					Platform.runLater(() -> error_msg.setText("서버 에러 혹은 인터넷 연결을 확인해주세요."));
-					Platform.runLater(() -> loginButton.setDisable(true));
-				}
-            });
-            listener.setDaemon(true);
-            listener.start();
+        if(ClientInfo.userId != null && !ClientInfo.userId.equals("") ) {
+        	Platform.runLater(() -> idTextField.setText(ClientInfo.userId));
+        	Platform.runLater(() -> login());
         }
-        
-        catch (IOException e) {
-        	Platform.runLater(() -> error_msg.setText("서버 에러 혹은 인터넷 연결을 확인해주세요."));
-        	Platform.runLater(() -> loginButton.setDisable(true));
-        }
-		
 	}
-
-}
 
 	@FXML public void login() {
 		if(!idTextField.getText().equals("")) {
